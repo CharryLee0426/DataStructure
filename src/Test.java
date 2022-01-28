2bb922f6cac49d32d5d6a97381863695063c7c35
lichen <lichen0426@stu.xidian.edu.cn>
2022-01-29 01:37:44 +0800

    First Commit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @author lichen
+ * @date    2022-01-29
+ * @description     这是用来测试所写类的功能的测试入口
+ * */
+public class Test {
+
+    // test
+    public static void main(String[] args) {
+
+    }
+}
